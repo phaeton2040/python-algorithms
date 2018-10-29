ee7b08464fb27350978ff3fd2abf84a677f0fbb1
Andrey Babitsyn <phaeton2040@gmail.com>
2018-10-29 13:13:47 +0300

    Tasks 1-9

--- HW-Lesson1/task_6.py
+++ HW-Lesson1/task_6.py
@@ -1,1 +1,11 @@
 # Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+letter_a_code = 96
+letter_z_code = 122
+
+letter_num = int(input('Введите номер буквы английского алфавита: ')) + letter_a_code
+
+if letter_a_code <= letter_num <= letter_z_code:
+    print(f'Буква на выбранном месте: {chr(letter_num)}')
+else:
+    print(f'Буквы на выбранном месте не существует')
